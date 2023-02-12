acb1dbed0786f627dd5cc8a5b38c802a9a593f50
Holms <samiycrutoyden@gmail.com>
2023-02-12 13:03:01 +0300

    Revork buttons & messages config

--- bot/bot.py
+++ bot/bot.py
@@ -3,58 +3,82 @@ from os.path import join
 from asyncio import run
 from database import create_connect
 from aiogram import Bot, Dispatcher, types
-from modules import get_token, get_lang, send
+from modules import get_token, get_lang, get_static
 from aiogram.utils.keyboard import ReplyKeyboardBuilder, KeyboardButton, InlineKeyboardBuilder
 from aiogram.types import ReplyKeyboardMarkup, InlineKeyboardButton, InlineKeyboardMarkup
 
 bot = Bot(token=get_token(), parse_mode="HTML")
 dp = Dispatcher()
+_type = type
 
 MESSAGES = {}
 FAQ_MESSAGES = {}
 MARKUPS = {}
 LANGUAGE_CODES = []
 
+
 def message_handle(m:types.Message, type):
     lang = get_lang(m, LANGUAGE_CODES)
     return MESSAGES[lang][type]['title'] == m.text
 
 
+async def send(message, type):
+
+    lang_code = get_lang(message, LANGUAGE_CODES)
+
+    current_message = MESSAGES[lang_code][type]
+    text = current_message.get('text')
+    markup = MARKUPS[lang_code].get(type)
+    print(markup)
+    if current_message.get('attachment'):
+        await bot.send_photo(
+            chat_id=message.from_user.id,
+            photo=get_static(current_message.get('attachment')),
+            caption=text,
+            reply_markup=markup.as_markup(resize_keyboard=True) if markup else None
+        )
+    else:
+        await bot.send_message(
+            chat_id=message.from_user.id,
+            text=text,
+            reply_markup=markup.as_markup(resize_keyboard=True) if markup else None
+        )
+
+
 async def load_buttons():
-    global LANGUAGE_CODES, MESSAGES
+    global LANGUAGE_CODES, MESSAGES, MARKUPS
 
     db = await create_connect()
 
     LANGUAGE_CODES = [i['code'] for i in await db.fetch('SELECT code FROM localisations ORDER BY id')]
-    messages = await db.fetch("SELECT * FROM messages INNER JOIN localisations l on l.id = messages.locale_id")
-    markups = await db.fetch("SELECT * FROM markups INNER JOIN buttons b on markups.id = b.markup_id INNER JOIN localisations l on l.id = b.locale_id")
+    messages = await db.fetch("SELECT * FROM messages INNER JOIN localisations l on l.id = messages.locale_id ORDER BY priority")
 
     await db.close()
 
-    for markup in markups:
-        MARKUPS.setdefault(markup['code'], {})
-        MARKUPS[markup['code']].setdefault(markup['type'], ReplyKeyboardBuilder())
-        MARKUPS[markup['code']][markup['type']].add(KeyboardButton(text=markup['title']))
-        MARKUPS[markup['code']][markup['type']].adjust(2)
-
     for m in messages:
+        MARKUPS.setdefault(m['code'], {})
+        MARKUPS[m['code']].setdefault('start', ReplyKeyboardBuilder())
+        MARKUPS[m['code']].setdefault('faq_main', InlineKeyboardBuilder())
+
+        if m['type'] in ['about', 'faq_main', 'service', 'ticket']:
+            MARKUPS[m['code']]['start'].add(KeyboardButton(text=m['title']))
+            MARKUPS[m['code']]['start'].adjust(2)
+        elif m['type'] == 'faq':
+            MARKUPS[m['code']]['faq_main'].add(InlineKeyboardButton(text=m['title'],callback_data='faq'))
+            MARKUPS[m['code']]['faq_main'].adjust(2)
+
         MESSAGES.setdefault(m['code'], {})
         MESSAGES[m['code']][m['type']] = {
             'attachment': m['attachment'],
-            'text': m['text']
+            'text': m['text'],
+            'title': m['title']
         }
-
 
 
 @dp.message(commands=['start'])
 async def process_start_command(message: types.Message):
-    lang_code = get_lang(message, LANGUAGE_CODES)
 
-    await bot.send_message(
-        chat_id=message.from_user.id,
-        text=MESSAGES[lang_code]['start']['text'],
-        reply_markup=MARKUPS[lang_code]['start'].as_markup(resize_keyboard=True)
-    )
+    await send(message, 'start')
 
     profile_pictures = await bot.get_user_profile_photos(message.from_user.id, limit=1)
     avatars = profile_pictures.photos
@@ -80,8 +104,18 @@ async def process_start_command(message: types.Message):
     await db.close()
 
 
+@dp.message(lambda m: message_handle(m,'about'))
+async def about_us(message: types.Message):
+
+    await send(message, 'about')
+
+
+@dp.message(lambda m: message_handle(m, 'faq_main'))
+async def faq_main(message:types.Message):
+    await send(message, 'faq_main')
 
 if __name__ == '__main__':
     run(load_buttons())
     print('RUN BOT')
+
     dp.run_polling(bot)
--- bot/modules.py
+++ bot/modules.py
@@ -15,22 +15,3 @@ def get_lang(m, LANGUAGE_CODES):
     return m.from_user.language_code if m.from_user.language_code in LANGUAGE_CODES else 'ru'
 
 
-async def send(bot, message, type, MAIN_MESSAGES, LANGUAGE_CODES, MARKUP):
-    lang_code = get_lang(message, LANGUAGE_CODES)
-
-    current_message = MAIN_MESSAGES[lang_code][type]
-    text = current_message.get('description')
-
-    if current_message.get('attachment'):
-        await bot.send_photo(
-            chat_id=message.from_user.id,
-            photo=get_static(current_message.get('attachment')),
-            caption=text,
-            reply_markup=MARKUP[type][lang_code].as_markup()
-        )
-    else:
-        await bot.send_message(
-            chat_id=message.from_user.id,
-            text=text,
-            reply_markup=MARKUP[type][lang_code].as_markup()
-        )
